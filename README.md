c8adc3a5a751bc8c0a1ca6c813710ba63e195abe
joanorrit <jorrit06@gmail.com>
2017-12-07 02:50:40 +0100

    First Commit

---
+++ README.md
@@ -0,0 +1,4 @@
+myproject
+=========
+
+A Symfony project created on November 29, 2017, 6:30 pm.
---
+++ src/AppBundle/Controller/LuckyController.php
@@ -0,0 +1,42 @@
+<?php
+// src/AppBundle/Controller/LuckyController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class LuckyController extends Controller
+{
+    /**
+     * @Route("/lucky/num")
+     */
+    public function numberAction()
+    {
+        $number = mt_rand(0, 100);
+
+        return $this->render('lucky/number.html.twig', [
+			'number' => $number,
+		]);
+    }
+}
+/*namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     *
+    public function indexAction(Request $request)
+    {
+        // replace this example code with whatever you need
+        return $this->render('default/index.html.twig', [
+            'base_dir' => realpath($this->getParameter('kernel.project_dir')).DIRECTORY_SEPARATOR,
+        ]);
+    }
+}*/
+
